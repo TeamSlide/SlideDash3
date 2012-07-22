553553613e99921d37a9067399f4e0cc9e86327e
David Liu <david.y1.liu@kp.org>
2012-07-22 04:06:28 -0700

    twitter and traffie done!%

---
+++ SlideDash/CommuteViewController.h
@@ -0,0 +1,13 @@
+//
+//  CommuteViewController.h
+//  SlideDash
+//
+//  Created by Mcoe mac on 7/22/12.
+//  Copyright (c) 2012 teamslide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommuteViewController : UIViewController
+
+@end
---
+++ SlideDash/CommuteViewController.m
@@ -0,0 +1,44 @@
+//
+//  CommuteViewController.m
+//  SlideDash
+//
+//  Created by Mcoe mac on 7/22/12.
+//  Copyright (c) 2012 teamslide. All rights reserved.
+//
+
+#import "CommuteViewController.h"
+
+@interface CommuteViewController ()
+
+@end
+
+@implementation CommuteViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
